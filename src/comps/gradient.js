82861b4e4bb6596b562ca4356e641050995f2355
Brad White <bradscottwhite@gmail.com>
2023-01-23 18:53:38 +0000

    added gradient

--- src/comps/gradient.js
+++ src/comps/gradient.js
@@ -1,6 +1,8 @@
 module.exports = {
   '.text-gradient-to-r': {
-    '@apply text-red-500': true,
+    '@apply text-transparent bg-clip-text': true,
+    '@apply bg-gradient-to-r': true,
+    '@apply from-primary to-secondary': true,
     '&:hover': {
       '@apply text-blue-300': true
     }
